781f662e1923b012de2b0bea449a11fc59bc15e6
kyildirim07 <kyildirim07@outlook.com>
2019-03-19 20:44:23 +0300

    Product and Category Entities Added

---
+++ Detur.Cms.Entities/Concrete/Product.cs
@@ -0,0 +1,17 @@
+﻿using Detur.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Detur.Cms.Entities.Concrete
+{
+   public class Product:IEntity
+    {
+        //if a class implement IEntity then that class is a entity(table)
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
--- Detur.Core/Entities/IEntity.cs
+++ Detur.Core/Entities/IEntity.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Detur.Cms.Entities
+namespace Detur.Core.Entities
 {
-    public class Class1
+    public interface IEntity
     {
     }
 }
